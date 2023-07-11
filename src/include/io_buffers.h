eb49495c680954e01e58470dd2515c10256da106
Hans-Christian Stadler <hans-christian.stadler@psi.ch>
2023-07-11 11:02:17 +0200

    Change io buffer collection to dynamic implementation

--- src/include/io_buffers.h
+++ src/include/io_buffers.h
@@ -1,66 +1,88 @@
 #ifdef IO_BUFFERS_H
 #define IO_BUFFERS_H
 
-#include <vector>
+#include <map>
+#include <list>
 #include <mutex>
 #include <condition_variable>
 
-static io_buffers final {
-    constexpr static unsigned NO_MORE_DATA = (unsigned)-1;
-    constexpr static unsigned NO_FREE_BUFFERS = (unsigned)-1;
-    std::vector<std::vector<char>> buffer;  // N buffers M bytes long
-    std::vector<unsigned> size = {0};       // buffer content sizes
-    unsigned num_nonempty_buffers = 0;      // buffers with size > 0
-    unsigned writing = 0;                   // index of buffer being filled
-    std::mutex modify_buffers;
-    std::condition_variable ready_for_reading;
-    bool no_more_data = 0;
-    bool insufficient_capacity = false;
+struct rw_buffer {
+    std::vector<char> content;
+    size_t size = 0;
 
-    // block on condvar if buffers empty and no_more_data is false
-    // return buffer index or NO_MORE_DATA if no_more_data is true
-    inline unsigned next_read_buffer()
+    rw_buffer(size_t sz)
+        : content{sz}
+    {}
+
+    rw_buffer(const rw_buffer&) = delete;
+    rw_buffer(rw_buffer&&) = default;
+    rw_buffer& operator=(const rw_buffer&) = delete;
+    rw_buffer& operator=(rw_buffer&&) = default;
+}
+
+struct io_buffers final {
+    static size_t buffer_size = 1024;
+    using buffer_type = std::map<uint64_t, std::unique_ptr<rw_buffer>>;
+    using element_type = buffer_type::value_type;
+    buffer_type buffer;
+    std::list<std::unique_ptr<rw_buffer>> free_list;
+    std::mutex modify_buffer;
+    std::condition_variable ready_for_reading;
+    std::mutex modify_free_list;
+    bool no_more_data = false;
+
+    // block on condvar if buffer empty and no_more_data is false
+    // rw_buffer pointer is nullptr if there is no more data
+    inline element_type get_nonempty_buffer()
     {
-        std::lock_guard lock(modify_buffers);
-        while (num_nonempty_buffers == 0) {
+        std::lock_guard lock(modify_buffer);
+        while (buffer.empty()) {
             if (no_more_data)
-                return NO_MORE_DATA;
+                return {0, nullptr};
             ready_for_reading.wait()
         }
-        unsigned index = (buffer.size() + writing - num_nonempty_buffers) % buffer.size();
-        num_nonempty_buffers--;
-        return index;
+        auto top = std::begin(buffer);
+        element_type res = std::move(*top);
+        buffer.erase(top);
+        return res;
     }
 
-    // return next write buffer index or NO_FREE_BUFFERS
-    inline unsigned commit_buffer()
+    inline void put_empty_buffer(std::unique_ptr<rw_buffer>&& buf)
     {
-        {
-            std::lock_guard lock(modify_buffers);
-            writing = (writing + 1) % buffer.size();
-            num_nonempty_buffers++;
-            ready_for_reading.notify_one();
-            insufficient_capacity = num_nonempty_buffers == buffer.size();
-        }
-        return insufficient_capacity ? NO_FREE_BUFFERS : writing;
+        std::lock_guard(modify_free_list);
+        free_list.push_front(buf);
     }
 
-    // check if there are free buffers (only necessary after commit_buffer() returns NO_FREE_BUFFERS)
-    inline bool ready_to_write()
+    inline std::unique_ptr<rw_buffer> get_empty_buffer()
     {
-        {
-            std::lock_guard lock(modify_buffers);
-            insufficient_capacity = (num_nonempty_buffers == buffer.size());
-        }
-        return !insufficient_capacity;
+        std::lock_guard(modify_free_list);
+        auto top = std::begin(free_list);
+        if (top = std::end(free_list))
+            return new rw_buffer{buffer_size};
+        auto res = std::move(*top);
+        free_list.pop_front();
+        return res;
+    }
+
+    inline void put_nonfree_buffer(element_type&& element)
+    {
+        std::lock_guard lock(modify_buffer);
+        buffer.insert(element);
+        ready_for_reading.notify_one();
     }
 
     inline void finish_writing()
     {
-        std::lock_guard lock(modify_buffers);
+        std::lock_guard lock(modify_buffer);
         no_more_data = true;
         ready_for_reading.notify_one();
     }
+
+    io_buffers() = default;
+    io_buffers(const io_buffers&) = delete;
+    io_buffers(io_buffers&&) = default;
+    io_buffers& operator=(const io_buffers&) = delete;
+    io_buffers& operator=(io_buffers&&) = default;
 };
 
 using io_buffer_collection = std::vector<std::unique_ptr<io_buffers>>;
