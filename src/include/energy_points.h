d4e13e1b35ecedd358f30c94ca52e8c4a80739f1
Hans-Christian Stadler <hans-christian.stadler@psi.ch>
2023-09-21 15:13:17 +0200

    Bugfix: add energy_points.h

---
+++ src/include/energy_points.h
@@ -0,0 +1,37 @@
+#ifndef ENERGY_POINTS_H
+#define ENERGY_POINTS_H
+
+struct EpPart final {
+        unsigned energy_point;                  //!< pixel contributes to this energy point
+        float weight;                           //!< with this weight
+};
+
+struct FlatPixelToEp final {
+        std::vector<EpPart> part;               //!< one part per energy point
+};
+
+struct ChipToEp final {
+        std::vector<FlatPixelToEp> flat_pixel;
+};
+
+struct PixelIndexToEp final {
+        std::vector<ChipToEp> chip;
+        unsigned npoints = 0;
+
+        inline FlatPixelToEp& operator[](const PixelIndex& index)
+        {
+                return chip[index.chip].flat_pixel[index.flat_pixel];
+        }
+
+        inline const FlatPixelToEp& operator[](const PixelIndex& index) const
+        {
+                return chip[index.chip].flat_pixel[index.flat_pixel];
+        }
+
+        inline FlatPixelToEp& at(const PixelIndex& index)
+        {
+                return chip.at(index.chip).flat_pixel.at(index.flat_pixel);
+        }
+};
+
+#endif // ENERGY_POINTS_H
