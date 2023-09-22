1cccc5aa948d2dbec6fb2fecddf6d1a19478d87e
Hans-Christian Stadler <hans-christian.stadler@psi.ch>
2023-09-22 17:26:15 +0200

    Add generate_map.jl to archive

--- archive.sh
+++ archive.sh
@@ -30,6 +30,6 @@ if [ -n "$PREPEND_DATE" ]; then
     BACKUP_FILE="${DNAME}/${PREPEND_DATE}-${FNAME}"
 fi
 
-CMD="tar czf $BACKUP_FILE src/*.cpp src/include/*.h compile.sh archive.sh generate_data/generate_data.jl generate_data/data"
+CMD="tar czf $BACKUP_FILE src/*.cpp src/include/*.h compile.sh archive.sh generate_data/generate_data.jl generate_data/generate_map.jl generate_data/data"
 echo "$CMD"
 eval "$CMD"
