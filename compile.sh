82eb759a03f651fb0ad0deaa3de57aa386207432
Hans-Christian Stadler <hans-christian.stadler@psi.ch>
2023-09-25 15:21:15 +0200

    Add help|--help|-h target

--- compile.sh
+++ compile.sh
@@ -22,14 +22,32 @@ case "$TARGET" in
         cmd="${CXX} -I src/include src/main.cpp src/processing.cpp -std=c++17 ${CXXFLAGS} ${LDFLAGS} -o tpx3app"
         echo "$cmd"
         eval "$cmd";;
-    "test")
-        cmd="${CXX} -I src/include src/test.cpp -std=c++17 ${TEST_FLAGS} -o test"
-        echo "$cmd"
-        eval "$cmd";;
     "server")
         cmd="${CXX} -I src/include src/test_server.cpp -std=c++17 ${CXXFLAGS} ${LDFLAGS} -o server"
         echo "$cmd"
         eval "$cmd";;
+    "test")
+        cmd="${CXX} -I src/include src/test.cpp -std=c++17 ${TEST_FLAGS} -o test"
+        echo "$cmd"
+        eval "$cmd";;
+    "help"|"--help"|"-h")
+        echo "Use: ${0} <target>"
+        echo
+        echo "Compile targets:"
+        echo "  tpx3app        (default) analysis application"
+        echo "  server         raw data replay server"
+        echo "  test           some unit tests for parts of the queueing code"
+        echo "Debendencies:"
+        echo "  ${LDFLAGS}"
+        echo "Environment:"
+        echo "  CXX            C++-17 and g++ options compatible compiler"
+        echo "  tpx3app, server:"
+        echo "    CXXFLAGS     extra compiler flags"
+        echo "    LDFLAGS      extra linker flags"
+        echo "    SPEED_FLAGS  extra optimization flags"
+        echo "    WARN_FLAGS   extra warning flags"
+        echo "  test:"
+        echo "    TEST_FLAGS   extra test executable flags";;
     *)
         echo "unknown target: $TARGET";;
 esac
