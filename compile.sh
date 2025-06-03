2d983dece2af8c846a1fdfd435b41fb7e03541ca
Hans-Christian Stadler <hans-christian.stadler@psi.ch>
2025-06-03 13:26:33 +0200

    Special code for Christian Appel

--- compile.sh
+++ compile.sh
@@ -17,6 +17,30 @@ fi
 
 CXXFLAGS+=" $WARN_FLAGS $SPEED_FLAGS"
 
+# Simplify compilation for Christian Appels Appel computer ;-)
+# If $POCO_DIR/include is a directory, take POCO_DIR as base,
+# otherwise assume that POCO_DIR/<some_version> is the base.
+# Try to find the highest version.
+if [ "$(uname)" == "Darwin" ]; then
+    if [[ "$CXXFLAGS" != *"poco"* ]]; then
+        poco_dir=${POCO_DIR:-"/opt/homebrew/Cellar/poco"}
+        if [ -d "$poco_dir/include" ]; then
+            CXXFLAGS="-I$poco_dir/include $CXXFLAGS"
+            LDFLAGS="-L$poco_dir/lib $LDFLAGS"
+        elif [ -d "$poco_dir" ]; then
+            poco_versions=($(ls "$poco_dir" | sort -n))
+            n_versions=${#poco_versions[*]}
+            if ((n_versions != 0)); then
+                poco_inst="$poco_dir/${poco_versions[$((n_versions-1))]}"
+                if [ -d "$poco_inst" ]; then
+                    CXXFLAGS="-I$poco_inst/include $CXXFLAGS"
+                    LDFLAGS="-L$poco_inst/lib $LDFLAGS"
+                fi
+            fi
+        fi
+    fi
+fi
+
 TEST_FLAGS+=" -Og -ggdb -march=native"
 
 test -f README.md || { echo "This command must be executed from the git top directory"; exit 1; }
@@ -52,6 +76,10 @@ case "$TARGET" in
         cmd="doxygen doc/doxygen.cfg"
         echo "$cmd"
         eval "$cmd";;
+    "flags")
+        echo "CXXFLAGS=$CXXFLAGS"
+        echo "LDFLAGS=$LDFLAGS"
+        echo "TEST_FLAGS=$TEST_FLAGS";;
     "help"|"--help"|"-h")
         echo "Use: ${0} <target>"
         echo
