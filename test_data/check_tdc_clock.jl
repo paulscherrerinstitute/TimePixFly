3ba69c802e19d07439ea3ef75d35ea0dec5904a8
Hans-Christian Stadler <hans-christian.stadler@psi.ch>
2026-06-03 14:43:07 +0200

    Add plotting and anomaly printout

--- test_data/check_tdc_clock.jl
+++ test_data/check_tdc_clock.jl
@@ -1,5 +1,7 @@
 using ArgParse
 using Statistics
+using WGLMakie
+using AlgebraOfGraphics
 
 function arg_parse()
     settings = ArgParseSettings(
@@ -12,6 +14,16 @@ function arg_parse()
         arg_type = String
         help = "input file name"
         required = true
+
+        "--plot", "-p"
+        metavar = "CHIP"
+        arg_type = Int64
+        default = -1
+
+        "--save", "-s"
+        metavar = "IMAGE"
+        arg_type = String
+        default = ""
     end
 
     args = parse_args(settings)
@@ -54,36 +66,61 @@ function parse_tdcs(tdcs::Vector{Float64}, events::AbstractArray{UInt64})
     end
 end
 
-function check_tdc_clock(events::AbstractArray{UInt64}, N::Int)
-    per_chip__tdcs = Dict{UInt64, Vector{Float64}}()
+function check_tdc_clock(events::AbstractArray{UInt64}, N::Int, plot::Int64, image_file::String)
+    per_chip_tdcs = Dict{UInt64, Vector{Float64}}()
 
     i = UInt64(1)
     while i <= N
         chip, id, size = parse_header(events, i)
 
-        if !haskey(per_chip__tdcs, chip)
-            per_chip__tdcs[chip] = Vector{Float64}()
+        if !haskey(per_chip_tdcs, chip)
+            per_chip_tdcs[chip] = Vector{Float64}()
         end
 
-        parse_tdcs(per_chip__tdcs[chip], view(events, i+2:i+size))
+        parse_tdcs(per_chip_tdcs[chip], view(events, i+2:i+size))
         i += size + 1
     end
 
     per_chip_tdc_diff = Dict{UInt64, Vector{Float64}}()
-    for (chip, tdcs) in pairs(per_chip__tdcs)
-        sort!(tdcs)
-        per_chip_tdc_diff[chip] = diff(tdcs)
+    for (chip, tdcs) in pairs(per_chip_tdcs)
+        tdc_diffs = diff(tdcs)
+        per_chip_tdc_diff[chip] = tdc_diffs
+        if chip == plot
+            fig = Figure(size = (600, 800))
+
+            println("plotting tdc differences for chip $chip ...")
+            ax1 = Axis(fig[1,1], title="TDC clock diffs", ylabel="tdc-diff")
+            lines!(ax1, eachindex(tdc_diffs), tdc_diffs)
+
+            println("plotting tdc timestamps for chip $chip ...")
+            ax2 = Axis(fig[2,1], title="TDC clock", xlabel="period", ylabel="clock")
+            lines!(ax2, eachindex(tdcs), tdcs)
+
+            display(fig)
+
+            if !isempty(image_file)
+                save(image_file, fig)
+            end
+        end
     end
 
     println("Statistic:")
-    for (chip, tdcs_diffs) in pairs(per_chip_tdc_diff)
-        s_len = length(per_chip__tdcs[chip])
-        s_mean = mean(tdcs_diffs)
-        s_max = maximum(tdcs_diffs)
-        s_min = minimum(tdcs_diffs)
-        s_median = median(tdcs_diffs)
-        s_stddev = std(tdcs_diffs)
+    for (chip, tdc_diffs) in pairs(per_chip_tdc_diff)
+        s_len = length(per_chip_tdcs[chip])
+        s_mean = mean(tdc_diffs)
+        s_max = maximum(tdc_diffs)
+        s_min = minimum(tdc_diffs)
+        s_median = median(tdc_diffs)
+        s_stddev = std(tdc_diffs)
         println("chip $chip -> $s_len tdcs, clock difference statistics: mean=$s_mean, median=$s_median, stddev=$s_stddev, min=$s_min, max=$s_max")
+
+        if s_max > 2*s_median
+            tdcs = per_chip_tdcs[chip]
+            indices = findall(tdc_diffs .> 2*s_median)
+            for i in indices
+                println("   at $i: diff ", tdc_diffs[i], " between tdcs (", tdcs[i], ", ", tdcs[i+1], ")")
+            end
+        end
     end
 end
 
@@ -100,7 +137,13 @@ function main()
     data = nothing
     N = length(events)
 
-    check_tdc_clock(events, N)
+    plot = args["plot"]
+    if plot >= 0
+        WGLMakie.activate!()
+    end
+    image_file = args["save"]
+
+    check_tdc_clock(events, N, plot, image_file)
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
